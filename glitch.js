e90965bb8f2372b7eb85611c88b3686a5da9ff35
Uniquebum <miroruopsa@gmail.com>
2019-02-05 01:19:16 +0200

    fine tuning header text

--- glitch.js
+++ glitch.js
@@ -11,7 +11,7 @@ const interval = 3000;
 //glitchy_el.style.position ="absolute";
 glitchy_el.style.left = `${50}%`;
 glitchy_el.style.transform = "translateX(-50%)";
-glitchy_el.style.top = `${360}px`;
+glitchy_el.style.top = `${45}%`;
 glitchy_el.style.color = "white";
 glitchy_el.style.fontSize = "26px";
 glitchy_el.style.fontWeight = "18px";
@@ -31,12 +31,6 @@ const g2 = document.createElement(elemType);
 g1.innerText = txt1;
 g2.innerText = txt2;
 
-/*
-document.querySelector("body").appendChild(txt_cp1);
-document.querySelector("body").appendChild(txt_cp2);
-document.querySelector("body").appendChild(g1);
-document.querySelector("body").appendChild(g2);
-*/
 div.appendChild(txt_cp1);
 div.appendChild(txt_cp2);
 div.appendChild(g1);
@@ -50,19 +44,17 @@ g2.style.visibility = "hidden";
 setTimeout(() => {
 const {x,y} = glitchy_el.getBoundingClientRect();
 
-console.log(glitchy_el.style.top)
-console.log(glitchy_el.style.left)
-const txt_cp1_x = 50;
-const txt_cp1_y = y-10;
+let txt_cp1_x = 50;
+let txt_cp1_y = y-10;
 
-const txt_cp2_x = 50;
-const txt_cp2_y = y-12;
+let txt_cp2_x = 50;
+let txt_cp2_y = y-12;
 
-const g1_x = x+100;
-const g1_y = y-32;
+let g1_x = 51;
+let g1_y = y-32;
 
-const g2_x = x+220;
-const g2_y = y-34;
+let g2_x = 51+(window.screen.availWidth/window.innerWidth)*5;
+let g2_y = y-34;
 /**
  * Default styles
  */
@@ -88,24 +80,35 @@ txt_cp2.style.visibility = "hidden";
 
 g1.style.color = "#FFFFFF";
 //g1.style.position = "absolute";
-g1.style.left = `${g1_x}px`;
+g1.style.left = `${g1_x}%`;
 g1.style.top = `${g1_y}px`;
 g1.style.fontSize = "26px";
 g1.style.fontWeight = "18px";
 g1.style.visibility = "hidden";
-g1.style.webkitTextStroke = ".5px green"
+g1.style.webkitTextStroke = ".75px green"
 
 g2.style.color = "#FFFFFF";
 //g2.style.position = "absolute";
-g2.style.left = `${g2_x}px`;
+g2.style.left = `${g2_x}%`;
 g2.style.top = `${g2_y}px`;
 g2.style.fontSize = "26px";
 g2.style.fontWeight = "18px";
 g2.style.visibility = "hidden";
-txt_cp2.style.webkitTextStroke = ".5px red"
+g2.style.webkitTextStroke = ".5px red"
 
 const createGlitch = async () => {
+    txt_cp1_x = 50;
+    txt_cp1_y = y-10;
     
+    txt_cp2_x = 50;
+    txt_cp2_y = y-12;
+    
+    g1_x = 51;
+    g1_y = y-32;
+    
+    g2_x = 51+(window.screen.availWidth/window.innerWidth)*5;
+    g2_y = y-34;
+
     txt_cp1.style.left = `${txt_cp1_x}%`;
     txt_cp1.style.top = `${txt_cp1_y}px`;
     txt_cp2.style.left = `${txt_cp2_x}%`;
@@ -118,6 +121,7 @@ const createGlitch = async () => {
         txt_cp1.style.left = `${txt_cp1_x-.2}%`;
         txt_cp2.style.visibility = "visible";
     });
+    
     await glitchPromise(50, () => {
         txt_cp1.style.visibility = "hidden";
         txt_cp2.style.left = `${txt_cp1_x+.2}%`;
@@ -127,7 +131,7 @@ const createGlitch = async () => {
     });
     await glitchPromise(50, () => {
         g1.style.top = `${g1_y-1}px`;
-        g1.style.left = `${g1_x+1}px`;
+        g1.style.left = `${g1_x+.1}%`;
     });
     await glitchPromise(25, () => {
         txt_cp1.style.visibility = "visible";
@@ -144,6 +148,8 @@ const createGlitch = async () => {
         txt_cp1.style.top = `${txt_cp1_y-2}px`;
         txt_cp2.style.left = `${txt_cp1_x-.1}%`;
         txt_cp2.style.top = `${txt_cp1_y-1}px`;
+
+        g2.style.left = `${g2_x+.1}%`;
     });
     await glitchPromise(50, () => {
         txt_cp1.style.visibility = "hidden";
